6f006d37b3aadf1b6c29b9798d65f5992b64a688
Zhiyu-Chen <chenzhiyu.aier@outlook.com>
2017-04-13 20:53:28 -0400

    csv

--- csv_extractor.py
+++ csv_extractor.py
@@ -9,7 +9,7 @@ import json
 class csv_extractor(object):
 	def __init__(self,iter_reader):
 		self.reader = iter_reader
-		self.rt_dict = dict()
+		self.rt_dict = dict() 
 		self.row_counter = 0
 		self.rows = []
 		for row in self.reader:
@@ -18,9 +18,16 @@ class csv_extractor(object):
 
 	
 	def size_extractor(self):
+		self.rt_dict['row_counts'] = self.row_counter
+
+
+	def entity_extractor(self,rt_dict):
 		pass
 
-	def entity_extractor(self):
-		pass
+
+	def extract_piplines(self):
+		self.size_extractor()
+		self.entity_extractor()
+		return self.rt_dict
 
 
