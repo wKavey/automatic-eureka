d9ead094919a564981c7a9d9c24f20854531c8d7
Zhiyu-Chen <chenzhiyu.aier@outlook.com>
2017-04-13 20:18:47 -0400

    csv

---
+++ csv_extractor.py
@@ -0,0 +1,12 @@
+class csv_extractor(object):
+	def __init__(self,iter_reader):
+		self.reader = iter_reader
+		self.rt_dict = dict()
+	
+	def size_extractor(self):
+		pass
+
+	def entity_extractor(self):
+		pass
+
+
