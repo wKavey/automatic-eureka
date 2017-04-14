e0c6d11828ce3d0b15405cc75e0c41d55c6d291a
Max <silverhammermba@gmail.com>
2017-04-13 21:04:54 -0400

    clean up a bit

--- csv_extractor.py
+++ csv_extractor.py
@@ -1,26 +1,23 @@
 import requests
 import os.path
 from urllib.parse import urlparse
-from contextlib import closing
-import csv
-import json
-
 
 class csv_extractor(object):
-	def __init__(self,iter_reader):
+	def __init__(self,resource,iter_reader):
+		self.resource = resource
 		self.reader = iter_reader
 		self.rt_dict = dict()
 		self.row_counter = 0
 		self.rows = []
 		for row in self.reader:
-		    self.rows.append(json.dumps(row))
-		    self.row_counter += 1
+			self.rows.append(row)
+			self.row_counter += 1
 
-	
 	def size_extractor(self):
 		pass
 
 	def entity_extractor(self):
 		pass
 
-
+	def metadata_extractor(self):
+		filename = os.path.basename(urlparse(resource['url']).path)
