d8bf42d21f2297120dfc141da0e9847238efa5df
Max <silverhammermba@gmail.com>
2017-04-16 15:40:07 -0400

    fix init for csv_extractor

--- scrape.py
+++ scrape.py
@@ -4,9 +4,6 @@ import csv
 import json
 from csv_extractor import csv_extractor
 
-'''
-1. how to get the title ?
-'''
 # simple script for getting/parsing CSVs
 
 base_url = 'http://catalog.data.gov/api/3'
@@ -28,18 +25,18 @@ index = 1
 # get results
 for page in range(pages):
     for result in action('package_search', params={'q': 'groups:education2168', 'rows': limit, 'start': page * limit})['result']['results']:
-        for resource in result['resources']:
+
+        # remove resources from result so we can just look at the dataset info on its own
+        resources = result['resources']
+        del result['resources']
+
+        for resource in resources:
             if resource['format'] == 'CSV' and resource['url'].endswith('.csv'):
                 with closing(requests.get(resource['url'], stream=True)) as request:
                     # TODO might get BOM in the first column header
                     reader = csv.reader(request.iter_lines(decode_unicode=True), delimiter=',', quotechar='"')
-                    ex = csv_extractor(resource, reader)
+                    ex = csv_extractor(result, resource, reader)
 
                     print(json.dumps({'index': {'_id': index}}))
                     print(json.dumps(ex.result(), cls=SetEncoder))
                     index += 1
-                # or write it to a file
-                #print(name)
-                #f = open(name, 'wb')
-                #f.write(requests.get(resource['url']).content)
-                #f.close()
