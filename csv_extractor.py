d8506c9b46f28a3f411caff5814102394a9a8ee0
Max <silverhammermba@gmail.com>
2017-04-16 10:15:16 -0400

    whitespace

--- csv_extractor.py
+++ csv_extractor.py
@@ -4,55 +4,55 @@ import json
 from nltk import word_tokenize
 
 class csv_extractor(object):
-	def __init__(self,resource,iter_reader):
-		self.resource = resource
-		self.reader = iter_reader
-		self.rt_dict = dict()
-		self.row_counter = 0
-		self.rows = []
-		for row in self.reader:
-			self.rows.append(row)
-			self.row_counter += 1
+    def __init__(self,resource,iter_reader):
+        self.resource = resource
+        self.reader = iter_reader
+        self.rt_dict = dict()
+        self.row_counter = 0
+        self.rows = []
+        for row in self.reader:
+            self.rows.append(row)
+            self.row_counter += 1
 
-	def size_extractor(self):
-		self.rt_dict['row_counts'] = self.row_counter
+    def size_extractor(self):
+        self.rt_dict['row_counts'] = self.row_counter
 
-	def table_text_extractor(self):
-		text = []
-		for row in self.rows:
-			for cell in row:
-				tokens = word_tokenize(cell)
-				for token in tokens:
-					if token.isalpha():
-						text.append(token)
-		self.rt_dict['text_tokens'] = list(set(text)) 
-
-
-
-	def entity_extractor(self):
-		'''
-		extract named entities from resource['description']
-		this function require Corenlp in server mode: https://stanfordnlp.github.io/CoreNLP/corenlp-server.html
-		'''
-		if not 'description' in self.resource:
-			return
-		desc = self.resource['description']
-		NERs = get_NERs(desc)
-		ner_text = []
-		for en_type in NERs:
-			ner_text.extend(NERs[en_type])
-		self.rt_dict['ners'] = ner_text
+    def table_text_extractor(self):
+        text = []
+        for row in self.rows:
+            for cell in row:
+                tokens = word_tokenize(cell)
+                for token in tokens:
+                    if token.isalpha():
+                        text.append(token)
+        self.rt_dict['text_tokens'] = list(set(text)) 
 
 
-	def metadata_extractor(self):
-		self.rt_dict['url'] = self.resource['url']
 
-	def extract_pipeline(self):
-		self.size_extractor()
-		self.entity_extractor()
-		self.metadata_extractor()
-		self.table_text_extractor()
-		return self.rt_dict
+    def entity_extractor(self):
+        '''
+        extract named entities from resource['description']
+        this function require Corenlp in server mode: https://stanfordnlp.github.io/CoreNLP/corenlp-server.html
+        '''
+        if not 'description' in self.resource:
+            return
+        desc = self.resource['description']
+        NERs = get_NERs(desc)
+        ner_text = []
+        for en_type in NERs:
+            ner_text.extend(NERs[en_type])
+        self.rt_dict['ners'] = ner_text
+
+
+    def metadata_extractor(self):
+        self.rt_dict['url'] = self.resource['url']
+
+    def extract_pipeline(self):
+        self.size_extractor()
+        self.entity_extractor()
+        self.metadata_extractor()
+        self.table_text_extractor()
+        return self.rt_dict
 
 
 def get_NERs(data = 'I love New York and California.'):
@@ -73,27 +73,27 @@ def get_NERs(data = 'I love New York and California.'):
         if token['ner'] != 'O':
             NER_type = token['ner']
             if NER_type in core_ners:
-	            if not NER_type in NERs :
-	                NERs[NER_type] = []
-	            NE = NE + token['word'] + ' '
-	            loc_count += 1
-	            #print token['word']
-	            idx += 1
-	            if idx < len(rs_json):
-	                token = rs_json[idx]
-	            else:
-	                NERs[NER_type].append(NE.strip())
-	                break
-	            while idx < len(rs_json) and token['ner'] == NER_type:
-	                #print token['word']
-	                NE = NE + token['word'] + ' '
-	                idx += 1
-	                if idx < len(rs_json):
-	                    token = rs_json[idx]
-	                else:
-	                    break
-	            #print NE
-	            if NER_type in core_ners:
-	            	NERs[NER_type].append(NE.strip())
+                if not NER_type in NERs :
+                    NERs[NER_type] = []
+                NE = NE + token['word'] + ' '
+                loc_count += 1
+                #print token['word']
+                idx += 1
+                if idx < len(rs_json):
+                    token = rs_json[idx]
+                else:
+                    NERs[NER_type].append(NE.strip())
+                    break
+                while idx < len(rs_json) and token['ner'] == NER_type:
+                    #print token['word']
+                    NE = NE + token['word'] + ' '
+                    idx += 1
+                    if idx < len(rs_json):
+                        token = rs_json[idx]
+                    else:
+                        break
+                #print NE
+                if NER_type in core_ners:
+                    NERs[NER_type].append(NE.strip())
         idx += 1
     return NERs
