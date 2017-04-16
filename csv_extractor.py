211960ecca46f5e3f040818a5072d863f2717bbf
Max <silverhammermba@gmail.com>
2017-04-16 14:10:10 -0400

    shorten decorator names, add CKAN dataset metadata
    also define a few more simple extractors

--- csv_extractor.py
+++ csv_extractor.py
@@ -5,8 +5,8 @@ import sys
 from nltk import word_tokenize
 from functools import wraps
 
-def per_row_extractor(func):
-    """A method decorator for methods of csv_extractor that do per-row metadata
+def per_row(func):
+    """A decorator for methods of csv_extractor that do per-row metadata
     extractions.
 
     The method must have the signature (self, key, row) and be called
@@ -35,19 +35,19 @@ def per_row_extractor(func):
 
     return wrapper
 
-def resource_extractor(func):
-    """A method decorator for methods of csv_extractor that do metadata
-    exraction from the CKAN resource.
+def metadata(func):
+    """A decorator for methods of csv_extractor that do metadata exraction from
+    the CKAN metadata.
 
-    The method must have the signature (self, resource) and be called
-    "something"_extractor. The method will be called with the CKAN resource of
-    the data set. Its return value will be automatically stored in
-    self.rt_dict["something"].
+    The method must have the signature (self, dataset, resource) and be called
+    "something"_extractor. The method will be called with the CKAN resource and
+    the dataset that resource is part of. Its return value will be
+    automatically stored in self.rt_dict["something"].
     """
     # bit of a hack to get class variables
     class_attrs = sys._getframe(1).f_locals
     suff = class_attrs.get('extractor_suffix')
-    exs = class_attrs.get('resource_extractors')
+    exs = class_attrs.get('metadata_extractors')
 
     # check name
     name = func.__name__
@@ -57,16 +57,22 @@ def resource_extractor(func):
     # update list of extractors
     exs.append(name)
 
-    # no need to wrap, just return original
-    return func
+    # wrap to store return value
+    @wraps(func)
+    def wrapper(self, *args):
+        val = func(self, *args)
+        if val is not None:
+            self.rt_dict[name[0:-len(suff)]] = val
+
+    return wrapper
 
 class csv_extractor(object):
     # keep a list of extractors to call on init
     per_row_extractors = []
-    resource_extractors = []
+    metadata_extractors = []
     extractor_suffix = '_extractor'
 
-    def __init__(self, resource, iter_reader):
+    def __init__(self, dataset, resource, iter_reader):
         self.rt_dict = dict()
 
         # pass each row to each row extractor
@@ -74,20 +80,19 @@ class csv_extractor(object):
             for ex in self.__class__.per_row_extractors:
                 getattr(self, ex)(row)
 
-        # then pass the CKAN resource to each resource extractor
-        for ex in self.__class__.resource_extractors:
-            # TODO don't store if return value is None?
-            self.rt_dict[ex[0:-len(self.__class__.extractor_suffix)]] = getattr(self, ex)(resource)
+        # then pass the CKAN data to each metadata extractor
+        for ex in self.__class__.metadata_extractors:
+            getattr(self, ex)(dataset, resource)
 
     def result(self):
         return self.rt_dict
 
-    @per_row_extractor
+    @per_row
     def row_count_extractor(self, key, row):
         """extract row count"""
         self.rt_dict[key] = self.rt_dict.get(key, 0) + 1
 
-    @per_row_extractor
+    @per_row
     def table_text_extractor(self, key, row):
         """extract set of unique tokens"""
         if key not in self.rt_dict:
@@ -99,8 +104,8 @@ class csv_extractor(object):
                 if token.isalpha():
                     self.rt_dict[key].add(token)
 
-    @resource_extractor
-    def NERs_extractor(self, resource):
+    @metadata
+    def NERs_extractor(self, dataset, resource):
         """extract named entities from the description
 
         this function require Corenlp in server mode: https://stanfordnlp.github.io/CoreNLP/corenlp-server.html
@@ -118,10 +123,38 @@ class csv_extractor(object):
             ner_text.extend(NERs[en_type])
         return ner_text
 
-    @resource_extractor
-    def url_extractor(self, resource):
+    @metadata
+    def url_extractor(self, dataset, resource):
         """extract the CSV URL"""
         return resource['url']
+
+    @metadata
+    def tag_extractor(self, dataset, resource):
+        try:
+            return list(map(lambda t: t['display_name'], dataset['tags']))
+        except KeyError:
+            return None
+
+    @metadata
+    def title_extractor(self, dataset, resource):
+        return dataset.get('title', None)
+
+    @metadata
+    def notes_extractor(self, dataset, resource):
+        return dataset.get('notes', None)
+
+    @metadata
+    def name_extractor(self, dataset, resource):
+        return resource.get('name', None)
+
+    @metadata
+    def date_extractor(self, dataset, resource):
+        # lots of possible dates, and they can be None
+        if resource.get('last_modified', None) is not None: return resource['last_modified']
+        if resource.get('created', None) is not None: return resource['created']
+        if dataset.get('metadata_modified', None) is not None: return dataset['metadata_modified']
+        if dataset.get('metadata_created', None) is not None: return dataset['metadata_created']
+        return None
 
 def get_NERs(data = 'I love New York and California.'):
     '''
